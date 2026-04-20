ff37b9206dd1e12f677b58516d52efb4534ff723
blue linden <dev@bluelinden.art>
2026-04-20 08:19:46 -0400

    apr 20 commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Logo from '$lib/logo.svelte';
+	import gradient from '$lib/assets/nirigradients.svg';
 </script>
 
 <main>
@@ -8,14 +9,34 @@
 			<ul>
 				<li id="logo"><Logo color="var(--color-text)" /></li>
 				<li><a href="/niri">Docs</a></li>
-				<li><a href="https://github.com/niri-wm/niri/">Source code</a></li>
+				<li><a href="https://github.com/niri-wm/niri/">Code</a></li>
 			</ul>
 		</menu>
 		<div class="colorful">
+			<video
+				src="https://github.com/niri-wm/niri/assets/1794388/bce834b0-f205-434e-a027-b373495f9729"
+				muted
+				autoplay
+				loop
+				playsinline
+				id="herovid"
+			></video>
+			<img src={gradient} id="herograd" alt="" aria-hidden="true" />
+			<script>
+				document.addEventListener('DOMContentLoaded', () => {
+					// set the video's playback speed to 2x
+					const video = document.getElementById('herovid');
+					video.playbackRate = 1.5;
+				});
+			</script>
 			<h1>Niri lets you scroll through your windows.</h1>
-			<a id="video">Watch showcase</a>
+			<h2>And it's fast, stable and usable beyond belief.</h2>
+			<a id="start" class="button skeuo">Start using it</a>
 		</div>
 	</header>
+	<section id="tiling">
+		<h1>How scrollable tiling works</h1>
+	</section>
 </main>
 
 <style>
@@ -38,28 +59,22 @@
 		flex-direction: column;
 		align-items: center;
 		justify-content: center;
-		padding: 4rem;
-		padding-top: 2rem;
+		padding: var(--space-lg);
+		padding-top: var(--space-md);
 	}
 
-	a#video {
-		background: var(--color-blue-900);
-		color: var(--color-pink-100);
-		font-family: 'National Park', sans-serif;
-		padding: 1rem 2rem;
-		border: none;
-		font-size: 2rem;
-		font-weight: 800;
-		border-radius: 999px;
+	a#start {
+		background: var(--color-blue-800);
+		color: var(--color-blue-200);
 	}
 
 	menu {
 		width: 100%;
 		display: flex;
 		justify-content: flex-end;
-		padding-left: 4rem;
-		margin-bottom: 2rem;
-		padding-right: 4rem;
+		padding-left: var(--space-xl);
+		margin-bottom: var(--space-md);
+		padding-right: var(--space-xl);
 		box-sizing: border-box;
 		top: 0;
 		left: 0;
@@ -74,22 +89,19 @@
 			list-style: none;
 			font-family: 'National Park', sans-serif;
 			font-weight: 800;
-			font-size: 1.5rem;
+			font-size: var(--text-md-nd);
 			a {
-				color: var(--color-pink-900);
-				@color-scheme dark {
-					color: var(--color-pink-100);
-					text-decoration-color: var(--color-pink-100);
-				}
-				text-decoration-thickness: 3px;
-				text-underline-offset: 6px;
+				color: var(--color-text);
+				text-decoration-thickness: 2.5px;
+				text-underline-offset: 4px;
 				text-decoration-inset: 2px;
 			}
 		}
 	}
 
 	.colorful {
-		background: linear-gradient(90deg, var(--color-pink-700), var(--color-orange-700));
+		/*background: linear-gradient(90deg, var(--color-pink-700), var(--color-orange-700));*/
+		background: var(--color-pink-800);
 		width: 100%;
 		height: 100%;
 		display: flex;
@@ -98,12 +110,57 @@
 		align-items: center;
 		justify-content: center;
 		border-radius: 32px;
+		overflow: hidden;
+		position: relative;
 		h1 {
-			font-size: 3rem;
+			font-size: var(--text-2xl);
 			font-family: 'National Park', sans-serif;
 			color: var(--color-pink-100);
 			font-weight: 800;
+			z-index: 1;
+			margin-bottom: calc(var(--space-lg) * -1);
 		}
+		h2 {
+			font-size: var(--text-xl);
+			font-family: 'National Park', sans-serif;
+			color: var(--color-pink-300);
+			font-weight: 800;
+			z-index: 1;
+		}
+		h1,
+		h2,
+		a {
+			margin-left: var(--space-md);
+			margin-right: var(--space-md);
+		}
+
+		#herovid {
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			transform: scale(101%);
+			z-index: 0;
+			object-fit: cover;
+			filter: brightness(30%) saturate(150%) blur(4px);
+		}
+		#herograd {
+			position: absolute;
+			height: 100%;
+			width: 100%;
+			z-index: 0;
+			mix-blend-mode: overlay;
+			object-fit: cover;
+		}
+	}
+
+	section > h1 {
+		font-size: var(--text-2xl);
+	}
+	
+	section#tiling {
+	  /* css grid blocks layout */
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
 	}
 
 	#logo {
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { islandsPlugin } from 'sveltekit-islands/plugin';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), islandsPlugin()],
   appType: 'mpa',
   css: {
-    
+
   }
 });
