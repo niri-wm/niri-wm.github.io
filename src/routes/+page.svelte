678e2e0e5927087546ddca744f1ad02dec6e693b
blue linden <dev@bluelinden.art>
2026-04-24 16:48:54 -0400

    add why niri section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
 	import Logo from '$lib/logo.svelte';
 	import gradient from '$lib/assets/nirigradients.svg';
+	import SplitscreenLandscapeAdd from 'virtual:icons/material-symbols/splitscreen-landscape-add-outline-rounded';
+	import Speed from 'virtual:icons/material-symbols/speed-outline-rounded';
+	import Style from 'virtual:icons/material-symbols/style-outline';
+	import TrackpadInput from 'virtual:icons/material-symbols/trackpad-input-outline-rounded';
+	import Exercise from 'virtual:icons/material-symbols/exercise-outline';
+	import Check from 'virtual:icons/material-symbols/check-rounded';
+	import OpenInFull from 'virtual:icons/material-symbols/open-in-full-rounded';
 </script>
 
 <main>
@@ -19,6 +26,7 @@
 				autoplay
 				loop
 				playsinline
+				class="blurred"
 				id="herovid"
 			></video>
 			<img src={gradient} id="herograd" alt="" aria-hidden="true" />
@@ -29,13 +37,105 @@
 					video.playbackRate = 1.5;
 				});
 			</script>
-			<h1>Niri lets you scroll through your windows.</h1>
+			<h1>niri lets you scroll through your windows.</h1>
 			<h2>And it's fast, stable and usable beyond belief.</h2>
 			<a id="start" class="button skeuo">Start using it</a>
+			<button id="fullscreen" class="button">
+				View demo
+				<OpenInFull width={32} height={32} />
+			</button>
+			<script>
+				document.getElementById('fullscreen').addEventListener('click', () => {
+					const video = document.getElementById('herovid');
+					if (video.requestFullscreen) {
+						video.requestFullscreen();
+						video.controls = true;
+						video.classList.remove('blurred');
+					} else if (video.webkitRequestFullscreen) {
+						/* Safari */
+						video.webkitRequestFullscreen();
+						video.controls = true;
+						video.classList.remove('blurred');
+					} else if (video.msRequestFullscreen) {
+						/* IE11 */
+						video.msRequestFullscreen();
+						video.controls = true;
+						video.classList.remove('blurred');
+					}
+				});
+				// restore video on close
+				document.addEventListener('fullscreenchange', () => {
+					const video = document.getElementById('herovid');
+					if (!document.fullscreenElement) {
+						video.controls = false;
+						video.classList.add('blurred');
+					}
+				});
+			</script>
 		</div>
 	</header>
 	<section id="tiling">
-		<h1>How scrollable tiling works</h1>
+		<h1>Why niri?</h1>
+		<div>
+			<figure>
+				<SplitscreenLandscapeAdd width={48} height={48} />
+
+				<h2>Your windows don't fight for space.</h2>
+				<p>
+					You can give each window as much space as it needs on your screen. Instead of stacking or
+					shrinking windows, niri moves them to the sides, off your screen until you need them.
+				</p>
+			</figure>
+			<figure>
+				<Speed width={48} height={48} />
+
+				<h2>You can put it on anything.</h2>
+				<p>
+					From a 2007 Eee PC to a modern gaming rig with high-performance graphics, niri supports a
+					lot. If it has video acceleration, it can probably run niri at 60 FPS.
+				</p>
+			</figure>
+			<figure>
+				<Style width={48} height={48} />
+
+				<h2>Looks how you like.</h2>
+				<p>
+					It supports fun eye candy like background blur and custom animations, as well as regular
+					customization options like borders, shadows and transparency.
+				</p>
+			</figure>
+			<figure>
+				<TrackpadInput width={48} height={48} />
+
+				<h2>You have controls.</h2>
+				<p>
+					niri supports trackpads, touchscreens, mice, trackballs, trackpoints, and drawing tablets,
+					and you can tweak how you use any of them without digging through dozens of config files.
+				</p>
+			</figure>
+			<figure>
+				<Exercise width={48} height={48} />
+
+				<h2>It won't brick.</h2>
+				<p>
+					You can depend on niri to update cleanly without breaking changes or crashes, so you can
+					focus on getting your work done. We will not break your config.
+				</p>
+			</figure>
+			<figure>
+				<Check width={48} height={48} />
+
+				<h2>niri just works.</h2>
+				<p>
+					It comes with trackpad gestures, a screenshot UI, an overview for your windows, screen
+					reader support, and alt-tab built right in. You can change all the settings in the niri
+					config file.
+				</p>
+			</figure>
+		</div>
+	</section>
+	<section id="users">
+
 	</section>
 </main>
 
@@ -64,8 +164,12 @@
 	}
 
 	a#start {
-		background: var(--color-blue-800);
-		color: var(--color-blue-200);
+		background: var(--color-blue-700);
+		color: var(--color-blue-100);
+
+		&:hover {
+			background: var(--color-blue-600);
+		}
 	}
 
 	menu {
@@ -101,7 +205,7 @@
 
 	.colorful {
 		/*background: linear-gradient(90deg, var(--color-pink-700), var(--color-orange-700));*/
-		background: var(--color-pink-800);
+		background: #000;
 		width: 100%;
 		height: 100%;
 		display: flex;
@@ -138,10 +242,13 @@
 			position: absolute;
 			height: 100%;
 			width: 100%;
-			transform: scale(101%);
+
 			z-index: 0;
-			object-fit: cover;
-			filter: brightness(30%) saturate(150%) blur(4px);
+
+			&.blurred {
+				filter: brightness(30%) saturate(150%) blur(4px);
+				transform: scale(101%);
+			}
 		}
 		#herograd {
 			position: absolute;
@@ -151,16 +258,48 @@
 			mix-blend-mode: overlay;
 			object-fit: cover;
 		}
+
+		#fullscreen {
+			bottom: var(--space-lg);
+			right: var(--space-lg);
+			background: var(--color-orange-300);
+			color: var(--color-orange-900);
+			&:hover {
+				background: var(--color-orange-200);
+			}
+			position: absolute;
+		}
 	}
 
 	section > h1 {
 		font-size: var(--text-2xl);
 	}
-	
+
 	section#tiling {
-	  /* css grid blocks layout */
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		/* css grid blocks layout */
+		display: flex;
+		flex-direction: column;
+		gap: var(--space-md);
+
+		> div {
+			display: grid;
+			max-width: min(var(--screen-md), 100vw);
+			grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+			gap: var(--space-md);
+			padding: var(--space-lg);
+			figure {
+				background-color: var(--color-background-container);
+				padding: var(--space-lg);
+				border-radius: 24px;
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				h2 {
+					line-height: var(--text-xl);
+					margin-bottom: var(--space-md);
+				}
+			}
+		}
 	}
 
 	#logo {
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { islandsPlugin } from 'sveltekit-islands/plugin';
+import Icons from 'unplugin-icons/vite'
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit(), islandsPlugin()],
+  plugins: [sveltekit(), islandsPlugin(), Icons({
+        compiler: 'svelte',
+      })],
   appType: 'mpa',
+  resolve: {
+    alias: [
+      // {
+      //   find: /^\$icons$/,
+      //   replacement: '/node_modules/@ajwdmedia/svelterial-symbols/Rounded'
+      // }
+    ]
+  },
   css: {
 
   }
